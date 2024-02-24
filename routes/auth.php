42b855bd7a04844189d6a4db94c57304a661612c
Mani Damirchilou <manidamirchilou@gmail.com>
2024-02-24 14:15:32 +0330

    Add Logout Feature

--- routes/auth.php
+++ routes/auth.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,9 +12,15 @@ Route::middleware('guest')->group(function (){
        Route::view('','auth.register')->name('register');
        Route::post('','store');
     });
+
     Route::prefix('login')->controller(LoginController::class)->group(function (){
         Route::view('','auth.login')->name('login');
         Route::post('','store');
     });
+
+});
+Route::middleware('auth')->group(function (){
+
+   Route::get('/logout',LogoutController::class)->name('logout');
 
 });
