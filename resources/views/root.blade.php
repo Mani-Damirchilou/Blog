2f781cb72d3a83b72149470e4d6c8da6da85454f
Mani Damirchilou <manidamirchilou@gmail.com>
2024-02-24 14:04:22 +0330

    Fix Title Bug

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -6,7 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/js/app.js'])
-    @yield('title')
+    <title>
+        @yield('title')
+    </title>
 </head>
 <body>
 @yield('body')
