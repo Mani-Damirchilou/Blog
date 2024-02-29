9efb975d72bc3ff235eb2e403b08b67bc8a12484
Mani Damirchilou <manidamirchilou@gmail.com>
2024-02-29 12:37:23 +0330

    Add Anti Spam Feature To Comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\StoreCommentRequest;
 use App\Models\Article;
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
 
 class CommentController extends Controller
 {
@@ -18,6 +20,14 @@ class CommentController extends Controller
 
     public function store(Article $article,StoreCommentRequest $request)
     {
+        if (RateLimiter::tooManyAttempts("user-{$request->user()->id}-commented",1))
+        {
+            throw ValidationException::withMessages([
+                'text' => __('auth.throttle',['seconds' => RateLimiter::availableIn("user-{$request->user()->id}-commented")])
+            ]);
+        }
+        RateLimiter::hit("user-{$request->user()->id}-commented",60);
+
         $article->comments()->create([
             'text' => $request->text,
             'user_id' => auth()->id()
